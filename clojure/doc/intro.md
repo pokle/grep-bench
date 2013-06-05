f905cfe06176f5d8b6ad1d8c0fcd0939dbe2501b
wpol <wpol>
2013-06-05 17:32:37 +1000

    Lein'd the project

---
+++ clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bench
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/src/bench/core.clj
@@ -0,0 +1,14 @@
+;
+; This manages 4mb/sec 100%cpu on my SSD Mac Mini :-(
+;
+; Example usage: lein run -m bench.core/grep root /etc/passwd
+;
+
+(ns bench.core)
+(use 'clojure.java.io)
+
+(defn grep [term file]
+  (with-open [rdr (reader file)]
+    (doseq [line (line-seq rdr)]
+      (if (. line startsWith term) (println line))
+    )))
