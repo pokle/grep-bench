71f531b92971a7b92d0f77df85f1113ce229d5b0
Tushar Pokle <tushar.pokle@gmail.com>
2013-10-15 22:14:58 +1100

    A tool to generate a large haystack (8gb) with one needle per gigabyte.

---
+++ benchmark/generate-test-data.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+TARGET=8 # Gigabytes
+
+NEEDLE="A very fine needle."
+
+function make_hay() {
+	# (length(HAY) + length("\n")) ** 24 == 1gb
+	HAY='Hey is hay the same as haay hey haai hei huh najfw and more hay'
+
+	mkdir -p data
+	echo $HAY > data/hay
+	for ((i=0; i<24; i++)); do
+		cat data/hay data/hay >> data/stack
+		mv data/stack data/hay
+	done
+}
+
+function make_haystack() {
+	rm -f data/haystack
+
+	for (( i=0; i<$TARGET; i++ )); do 
+		cat data/hay >> data/haystack
+		echo $NEEDLE >> data/haystack
+		du -h data/haystack
+	done
+}
+
+make_hay
+make_haystack
