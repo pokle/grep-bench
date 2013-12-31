a4954c1397b1f4eded27cef35fdd10b1a89f1ec5
Tushar Pokle <tushar.pokle@gmail.com>
2013-12-31 18:45:16 +1100

    Fixed formatting issues

--- cpp/cppgrep.cc
+++ cpp/cppgrep.cc
@@ -4,24 +4,25 @@
  
 int main( int argc , char** argv ) {
 
-  if (argc != 3) {
+  if ( argc != 3 ) {
     std::cout << "usage: " << argv[0] << " TOKEN FILE" << '\n';
     return 1;
   }
 
 
-  std::string token(argv[1]);
+  std::string token( argv[1] );
 
-   std::string line ;
-   std::ifstream infile( argv[ 2 ] ) ;
+  std::string line;
+  std::ifstream infile( argv[ 2 ] );
 
-   if ( infile ) {
-      while ( getline( infile , line ) ) {
-        if (std::string::npos != line.find(token)) {
-          std::cout << line << '\n';
-        }
+  if ( infile ) {
+    while ( getline( infile , line ) ) {
+      if ( std::string::npos != line.find( token ) ) {
+        std::cout << line << '\n';
       }
-   }
-   infile.close() ;
-   return 0 ;
+    }
+  }
+  
+  infile.close();
+  return 0;
 }
